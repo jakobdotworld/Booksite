c8de32075b5fc73f880c3bc9c3e49469b56a2b46
Jake Edwards <jakobmedwards@gmail.com>
2024-09-21 00:02:04 -0700

    third booksite update

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+    airtableBaseURL: 'https://api.airtable.com/v0/appyz0QNu3GzhgJ4L/CHARACTERS',
+    airtableAPIKey: 'patvEmrk5qMtFVQt0.df87b64f035800932be432bf2d48ab2a68d351324b89c1a74bd03e3ca7609779'
+};
+
+export default config;
